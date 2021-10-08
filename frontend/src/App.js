87d008d866ad8573fa215fd960eb76a6538bbf44
Avinash7564 <avinashkumarmbns@gmail.com>
2021-10-09 01:07:08 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Landing from "./Components/Landing/landingPage";
+import Routes from "./Routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Routes />
     </div>
   );
 }
--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -1,13 +1,18 @@
 import { Switch, Route } from "react-router-dom"
 import { Temp } from "../Components/temp"
+import Landing from "../Components/Landing/landingPage"
 
-
-export const Routes = () => {
+ const Routes = () => {
     return (
         <Switch>
+            <Route exact path="/">
+                <Landing/>
+            </Route>
             <Route exact path="/temp">
                 <Temp/>
             </Route>
         </Switch>
     )
 }
+
+export default Routes
